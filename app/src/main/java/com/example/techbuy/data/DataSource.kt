4aa4611ed209c9ca595c90e2841dc5d1944d1926
Kaveesha Senarathne <kaveeshagimhan49@gmail.com>
2025-06-07 19:49:57 +0530

    Add Products && git push

--- app/src/main/java/com/example/techbuy/data/DataSource.kt
+++ app/src/main/java/com/example/techbuy/data/DataSource.kt
@@ -6,8 +6,13 @@ import com.example.techbuy.R
 object DataSource {
     fun getProducts(): List<Product> {
         return listOf(
-            Product(1, "iPhone 13", 999.99, R.drawable.iphone),
-            Product(2, "MacBook Pro", 1999.99, R.drawable.macbook)
+            Product(1, "iPhone 16", 999.99, R.drawable.iphone16),
+            Product(2, "iPhone 16 Pro", 1199.99, R.drawable.iphone16p),
+            Product(3, "iPhone 16 Pro Max", 1299.99, R.drawable.iphone16pm),
+            Product(4, "MacBook M1", 1999.99, R.drawable.macbookm1),
+            Product(5, "MacBook M2", 2599.99, R.drawable.macbookm2),
+            Product(6, "MacBook M3", 2999.99, R.drawable.macbookm3),
+            Product(7, "MacBook M4", 3599.99, R.drawable.macbookm4),
         )
     }
 }
