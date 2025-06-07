df20059fec501b7c0e08057756436c5fa8ddd598
Kaveesha Senarathne <kaveeshagimhan49@gmail.com>
2025-06-07 23:40:02 +0530

    Update profile view in data source

--- app/src/main/java/com/example/techbuy/data/DataSource.kt
+++ app/src/main/java/com/example/techbuy/data/DataSource.kt
@@ -120,8 +120,8 @@ object DataSource {
     fun getSampleUser(): User {
         return User(
             id = 1,
-            name = "Jane Doe",
-            email = "jane.doe@example.com",
+            name = "Kaveesha Senarathne",
+            email = "kaveesha@techbuy.com",
             password = "samplePassword123" // Password field exists, so providing a sample
         )
     }
