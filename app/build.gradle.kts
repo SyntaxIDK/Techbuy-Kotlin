a86e41b14134e9c8444815cbae7a2135a0e1cd83
Kaveesha Senarathne <kaveeshagimhan49@gmail.com>
2025-06-08 09:26:03 +0000

    Fix: Add androidx.appcompat:appcompat dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended:1.6.7")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
